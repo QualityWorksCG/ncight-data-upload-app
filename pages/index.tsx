4c327b02b5b04ff4bca0bc86049db064779c6308
dscotland <dscotland@qualityworkscg.com>
2022-11-18 09:47:29 -0500

    chore: Text Update 1. Updated landing page text to include environment.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>
-        Welcome to the nCight QA Environment
+        Welcome to the nCight Staging Environment
       </h1>
 
       <p className={styles.description}>
