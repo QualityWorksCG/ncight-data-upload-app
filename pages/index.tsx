be2d0f6b0764c43767af9b324e453dde30ba1fb3
dscotland <dscotland@qualityworkscg.com>
2022-11-24 16:49:36 -0500

    feat: Authentication 1. Started implementing base Login Page. 2. Started Implementing Sigup Page based on Figma design. 3. Started integrating Amplify Auth Sign up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import type { NextPage } from 'next'
 import Router from 'next/router'
 import styles from '../styles/page.module.css'
+import LoginForm from '../components/auth/LoginForm';
+import SignUpForm from '../components/auth/SignUpForm';
+import {
+  Flex,
+  Stack,
+  VStack,
+  HStack,
+  Image,
+  Show,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Heading,
+  Box
+} from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   const moveToRequireAuthenticationPage = () => {
@@ -8,15 +25,40 @@ const Home: NextPage = () => {
   }
 
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to the nCight Development Environment
-      </h1>
-
-      <p className={styles.description}>
-        Move to the administrator screen: <button onClick={moveToRequireAuthenticationPage}>admin</button>
-      </p>
-    </main>
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} bg='background.main'>
+    <Show above='lg'>
+    <Flex flex={1}>
+        <Image
+          alt={'Sign up Image'}
+          objectFit={'cover'}
+          src={
+            'https://drive.google.com/uc?id=10o5dt6LjNRdlLkjepmKK9ALU4Y1epgHs'
+          }
+        />
+    </Flex>
+    </Show>
+    <Flex flex={1} align={'center'} justify={'center'} bg='background.main'>
+      <VStack spacing={8} color="primary.gray">
+          <Heading>Welcome to nCight!</Heading>
+      <Tabs variant='unstyled' size='lg' >
+          <TabList w={'2xl'}>
+            <HStack borderTopRadius='6px' bg='background.tabs'>
+              <Tab w={'10rem'} _selected={{ bg: 'secondary.yellow', borderTopLeftRadius:'md' }}>Login</Tab>
+              <Tab w={'10rem'} _selected={{ bg: 'secondary.yellow', borderTopRightRadius:'md' }}>Sign Up</Tab>
+            </HStack>
+          </TabList>
+        <TabPanels bg='background.tabs'>
+          <TabPanel>
+            <LoginForm/>
+          </TabPanel>
+          <TabPanel>
+            <SignUpForm/>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      </VStack>
+    </Flex>
+  </Stack>
   )
 }
 
---
+++ styles/theme.ts
@@ -0,0 +1,67 @@
+import { extendTheme, StyleFunctionProps, theme } from "@chakra-ui/react";
+import { League_Spartan } from '@next/font/google'
+
+const leagueSpartan = League_Spartan({
+    subsets: ['latin'],
+})
+
+export const customTheme = extendTheme({
+    fonts:{
+        body: leagueSpartan,
+    },
+    components: {
+        Input: {
+          variants: {
+            outline: (props: StyleFunctionProps) => ({
+              ...theme.components.Input.variants!.outline(props),
+              field: {
+                ...theme.components.Input.variants!.outline(props).field,
+                borderRadius: "6px",
+                color: "#D8DADA",
+              },
+            })
+          },
+          defaultProps:{
+            focusBorderColor: "#F09E28"
+        }
+        },
+        Select: {
+            defaultProps:{
+                focusBorderColor: "#F09E28",
+            }
+        },
+        Box: {
+            defaultProps:{
+                bg: "#F09E28",
+            }
+        },
+        Checkbox: {
+            color: '#F09E28'
+        }
+      },
+    fontSizes:{
+        sm: '0.875rem'
+    },
+    colors:{
+        background: {
+            main:'#2B2B2B',
+            tabs: '#3B3B3B'
+        },
+        primary: {
+            gray: '#D8DADA',
+            gray_blue: '#455469',
+            red: '#D80916',
+            white: '#FFFFFF'
+        },
+        secondary: {
+            red_orange: '#E3481D',
+            orange: '#FD7B25',
+            yellow: '#F09E28',
+        },
+        font: {
+            black: '#000000',
+            gray: '#455469',
+            orange: '#F96421'
+        }
+    }
+});
