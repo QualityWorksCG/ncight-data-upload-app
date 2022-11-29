778bfd45ff0b0b8d625a2ac95cef23c71107d683
dscotland <dscotland@qualityworkscg.com>
2022-11-28 22:14:20 -0500

    feat: Infrastructure 1. Updated nginx configuration

--- pages/verifyEmail.tsx
+++ pages/verifyEmail.tsx
@@ -1,4 +1,4 @@
-import BaseLogoPage from '../components/general/baseLogoPage';
+import BaseLogoPage from '../components/general/BaseLogoPage';
 import VerificationPageContent from '../components/auth/VerificationPageContent';
 import React,{ useState }from 'react';
 
